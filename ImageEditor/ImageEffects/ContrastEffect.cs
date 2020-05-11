30b43cdad048d6b843d50de4a50874db1def3a5a
John Smithers <john.smith@gironsoftware.com>
2020-05-11 20:08:19 +0200

    Optimize contrasteffect

--- ImageEditor/ImageEffects/ContrastEffect.cs
+++ ImageEditor/ImageEffects/ContrastEffect.cs
@@ -1,6 +1,7 @@
 ﻿using ImageEditor.Helper;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Threading.Tasks;
 
 namespace ImageEditor.ImageEffects
 {
@@ -24,13 +25,13 @@ namespace ImageEditor.ImageEffects
         /// </summary>
         /// <param name="img">The image to apply an effect to</param>
         /// <returns>A new image with the effect applied</returns>
-        public Bitmap ApplyEffect( Bitmap img )
+        public Bitmap ApplyEffect( Bitmap inImg )
         {
-            Bitmap contrastImg = (Bitmap)img.Clone();
-            BitmapData data = contrastImg.LockBits(new Rectangle(0, 0, contrastImg.Width, contrastImg.Height), ImageLockMode.ReadWrite, contrastImg.PixelFormat);
-
-            // 3 or 4 depending on if the image has alpha
-            int colWidth = data.Stride / contrastImg.Width;
+            Bitmap outImg = (Bitmap)inImg.Clone();
+            BitmapData outData = outImg.LockBits(new Rectangle(0, 0, outImg.Width, outImg.Height), ImageLockMode.ReadWrite, outImg.PixelFormat);
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(outImg.PixelFormat) / 8;
+            int height = outData.Height;
+            int width = outData.Width * bytesPerPixel;
             int contrast = (int)MathHelper.Lerp(-255.0f, 255.0f, Amount);
             float correctionFactor = (259.0f * (255.0f + contrast)) / (255.0f * (259.0f - contrast));
 
@@ -38,27 +39,25 @@ namespace ImageEditor.ImageEffects
             //to be useable since I'm using sliders which causes this function to be called frequently
             unsafe
             {
-                for( int y = 0; y < contrastImg.Height; ++y )
+                byte* ptr0 = (byte*)outData.Scan0;
+                Parallel.For( 0, height, y =>
                 {
-                    byte* row = (byte*)data.Scan0 + (y * data.Stride);
-                    int columnOffset = 0;
-                    for( int x = 0; x < contrastImg.Width; ++x )
+                    byte* row = ptr0 + (y * outData.Stride);
+                    for( int x = 0; x < width; x += bytesPerPixel )
                     {
-                        byte b = row[columnOffset];
-                        byte g = row[columnOffset + 1];
-                        byte r = row[columnOffset + 2];
+                        byte b = row[x];
+                        byte g = row[x + 1];
+                        byte r = row[x + 2];
 
-                        row[columnOffset + 2] = (byte)MathHelper.Clamp(0, 255, (int)(correctionFactor * ( r - 128.0f ) + 128.0f));
-                        row[columnOffset + 1] = ( byte )MathHelper.Clamp( 0, 255, ( int )( correctionFactor * ( g - 128.0f ) + 128.0f ) );
-                        row[columnOffset] = ( byte )MathHelper.Clamp( 0, 255, ( int )( correctionFactor * ( b - 128.0f ) + 128.0f ) );
-
-                        columnOffset += colWidth;
+                        row[x + 2] = ( byte )MathHelper.Clamp( 0, 255, ( int )( correctionFactor * ( r - 128.0f ) + 128.0f ) );
+                        row[x + 1] = ( byte )MathHelper.Clamp( 0, 255, ( int )( correctionFactor * ( g - 128.0f ) + 128.0f ) );
+                        row[x] = ( byte )MathHelper.Clamp( 0, 255, ( int )( correctionFactor * ( b - 128.0f ) + 128.0f ) );
                     }
-                }
+                } );
             }
 
-            contrastImg.UnlockBits( data );
-            return contrastImg;
+            outImg.UnlockBits( outData );
+            return outImg;
         }
     }
 }
